1da6e283189572666d8950702fb79d55629301f7
lekha15j <lekha15j@gmail.com>
2023-02-26 21:53:16 +0530

    .

--- college-connect/routes/api.php
+++ college-connect/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/banners', [\App\Http\Controllers\TestController::class, 'getBannerIndex']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
